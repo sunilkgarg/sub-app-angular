4bf88a4a37317a259ddc1584638beb4a820a67eb
sunilkgarg <sunilgarg1988@gmail.com>
2019-12-03 23:14:23 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'sub-app-angular';
+  user;
+  fakeUser = {
+    username: 'etest@example.com',
+    displayName: 'Test Test'
+  };
+
+  ngOnInit(): void {
+
+    this.user = JSON.parse(sessionStorage.getItem('USER') || JSON.stringify(this.fakeUser));
+  }
+
+  toggleClock () {
+    const event = new Event('toggleClock');
+    window.dispatchEvent(event);
+  }
+
+}
